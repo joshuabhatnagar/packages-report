4c3e7c1db205987f34118272dd153904acfe2aa0
Joshua Bhatnagar <joshua.bhatnagar@gmail.com>
2019-01-15 19:41:04 -0800

    script to create table of add-on packages and frequency table

---
+++ scripts/02_wrangle-packages.R
@@ -0,0 +1,23 @@
+## remember to restart R here!
+library(tidyverse)
+library(here)
+
+## create a data frame by reading from data/installed-packages.csv
+myInstalledPackages <- read_csv(here::here("data", "installed-packages.csv"))
+
+## filter out the base and recommended packages
+## keep the variables Package and Built
+myAddOnPackages <- myInstalledPackages %>%
+  filter(is.na(Priority)) %>%
+  select(Package, Built)
+
+## write this new, smaller data frame to data/add-on-packages.csv
+write_csv(myAddOnPackages, here::here("data", "add-on-packages.csv"))
+
+## make a frequency table of package by the version in Built
+myAddOnPackages_freqtable <- myUniquePackages %>%
+  count(Built) %>%
+  mutate(prop = n / sum(n))
+
+## write this data frame to data/add-on-packages-freqtable.csv
+write_csv(myAddOnPackages_freqtable, here::here("data", "add-on-packages-freqtable.csv"))
